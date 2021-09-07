4f1e332e02882650654c250ff46cb27d292ffc76
t.kanin <tan.kanin98@gmail.com>
2021-09-07 16:43:38 +0700

    add email validation and user rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  validates :email, uniqueness: { case_senstivie: false }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
