e34a1349a349006dc9b92769fd1099ea117cd70b
t.kanin <tan.kanin98@gmail.com>
2021-09-08 01:10:55 +0700

    Implement open and close tasks index

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks do
+    get :index_open_tasks, on: :collection
+    get :index_close_tasks, on: :collection
     resources :comments, only: %i[new create destroy]
   end
+
   root to: 'tasks#index'
 end
