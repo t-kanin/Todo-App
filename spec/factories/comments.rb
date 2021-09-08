11fd8e160191f5041ac611c02ae6e1841491a648
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-08 10:03:00 +0700

    Validation rspec for comment model (#6)

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    comment { "MyString" }
+    comment { 'My Comment' }
+    association :user
+    association :task
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    let(:comment) { build :comment }
+
+    it 'has valid object' do
+      expect(comment).to be_valid
+    end
+
+    it 'returns invalid when no user' do
+      comment.user = nil
+      expect(comment).not_to be_valid
+    end
+
+    it 'returns invalid when no task' do
+      comment.task = nil
+      expect(comment).not_to be_valid
+    end
+  end
 end
