eb4fdc7a410b94a9be5e836ee286c0adaed81fe3
Kanin Tansirisittikul <61454485+t-kanin@users.noreply.github.com>
2021-09-09 10:46:11 +0700

    Fixed routes (#11)
    - update actions name
    - rename files
    - refactor routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }, path_names: { sign_in: :login }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tasks do
-    get :index_open_tasks, on: :collection
-    get :index_close_tasks, on: :collection
+    collection do
+      get :index_open
+      get :index_close
+    end
     resources :comments, only: %i[index new create destroy]
   end
 
